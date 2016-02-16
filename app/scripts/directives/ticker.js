0bbcf3ecbe160347f360013165c0cd2dc21816b5
ttbarnes <tony@tonybarnes.me>
2016-02-16 09:08:06 +0000

    tidy indentation

--- app/scripts/directives/ticker.js
+++ app/scripts/directives/ticker.js
@@ -1,93 +1,93 @@
 'use strict';
 
 angular.module('simpleAngularTicker', []).
-directive('ticker', function ($interval, $timeout) {
-    return {
+directive('ticker', function ($ainterval, $timeout) {
+  return {
 
-        restrict: 'A',
-        scope: true,
-        compile: function () {
+    restrict: 'A',
+    scope: true,
+    compile: function () {
 
-            return function (scope, element, attributes) {
+      return function (scope, element, attributes) {
 
-                var timing,
-                    timingEffect,
-                    timingEffectDivideBy = 4,
-                    isHovered = false,
-                    innerTime,
-                    start;
+        var timing,
+        timingEffect,
+        timingEffectDivideBy = 4,
+        isHovered = false,
+        innerTime,
+        start;
 
-                if (attributes.timing) {
-                    timing = attributes.timing;
-                    timingEffect = timing / timingEffectDivideBy;
-                } else {
-                    timing = 5000;
-                    timingEffect = timing / timingEffectDivideBy / timingEffectDivideBy * 2;
-                }
-
-                scope.$watch(element, function () {
-
-                    var list = element,
-                        items = element.find('li'),
-                        itemFirst;
-
-
-                    if (items.length) {
-                        list.addClass('active');
-
-                        start = $interval(function () {
-
-                            /*cancel the callback function for fade-out and makes the ticker steady.*/
-                            if (isHovered) {
-                                $timeout.cancel(innerTime);
-                                return;
-                            }
-
-                            items = list.children('li');
-                            itemFirst = angular.element(items[0]);
-
-                            itemFirst.addClass('fade-out minus-margin-top');
-
-                            $timeout(function () {
-                                itemFirst.removeClass('minus-margin-top');
-                                list.append(itemFirst);
-
-                                innerTime = $timeout(function () {
-                                    items.removeClass('fade-out');
-                                }, timingEffect);
-
-                            }, timingEffect);
-
-                        }, timing);
-
-                    } else {
-                        console.warn('no items assigned to ticker! Ensure you have correctly assigned items to your ng-repeat.');
-                    }
-
-                });
-
-                element.on('$destroy', function () {
-                    $interval.cancel(start, 0);
-                });
-
-                /* 
-                 *author - mayo
-                 *checking for mouse enter the ticker region
-                 */
-                element.on('mouseenter', function () {
-                    isHovered = true;
-                });
-
-                /* 
-                 *author - mayo
-                 *checking for mouse exit the ticker region
-                 */
-                element.on('mouseleave', function () {
-                    isHovered = false;
-                });
-
-            };
+        if (attributes.timing) {
+          timing = attributes.timing;
+          timingEffect = timing / timingEffectDivideBy;
+        } else {
+          timing = 5000;
+          timingEffect = timing / timingEffectDivideBy / timingEffectDivideBy * 2;
         }
 
-    };
+        scope.$watch(element, function () {
+
+          var list = element,
+          items = element.find('li'),
+          itemFirst;
+
+
+          if (items.length) {
+            list.addClass('active');
+
+            start = $interval(function () {
+
+              /*cancel the callback function for fade-out and makes the ticker steady.*/
+              if (isHovered) {
+                $timeout.cancel(innerTime);
+                return;
+              }
+
+              items = list.children('li');
+              itemFirst = angular.element(items[0]);
+
+              itemFirst.addClass('fade-out minus-margin-top');
+
+              $timeout(function () {
+                itemFirst.removeClass('minus-margin-top');
+                list.append(itemFirst);
+
+                innerTime = $timeout(function () {
+                  items.removeClass('fade-out');
+                }, timingEffect);
+
+              }, timingEffect);
+
+            }, timing);
+
+          } else {
+            console.warn('no items assigned to ticker! Ensure you have correctly assigned items to your ng-repeat.');
+          }
+
+        });
+
+        element.on('$destroy', function () {
+          $interval.cancel(start, 0);
+        });
+
+        /*
+         *author - mayo
+         *checking for mouse enter the ticker region
+         */
+        element.on('mouseenter', function () {
+          isHovered = true;
+        });
+
+        /*
+         *author - mayo
+         *checking for mouse exit the ticker region
+         */
+        element.on('mouseleave', function () {
+          isHovered = false;
+        });
+
+      };
+    }
+
+  };
 });
